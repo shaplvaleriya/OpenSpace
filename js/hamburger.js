e68605bf7a843521d0cf4768714920050e9b01ed
Evgeny Smirnov <smirnof.evg@gmail.com>
2020-04-05 20:26:43 +0300

    header

---
+++ js/hamburger.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    var viewportWidth = $(window).width();
+    if (viewportWidth <= 768) {
+        $('#signInModal').removeClass('pater');
+        $('.nav-content').css('margin-left','-270px');
+        $('#signInModal').append("ВОЙТИ")
+    }else if (viewportWidth >= 768){
+        $('#signInModal').addClass('pater')
+        $('.nav-content').css('margin-left','0');
+    }
+    
+    $(".hamburger").click(function () {
+        $(this).toggleClass("is-active");
+        if(document.getElementById('hamburger-1').classList[1]) {
+            $('.nav-content').animate({
+                marginLeft: '-70px'
+            }, 400, () => {})
+        } else {
+            $('.nav-content').animate({
+                marginLeft: '-270px'
+            }, 400, () => {})
+        }
+    });
+});
+
+$(window).resize(function () {
+    var viewportWidth = $(window).width();
+    if (viewportWidth <= 768) {
+        $('#signInModal').removeClass('pater');
+        $('.nav-content').css('margin-left','-270px');
+        if(!$('#signInModal').children().prevObject['0'].innerText){
+            $('#signInModal').append("ВОЙТИ")
+        }
+    }else if (viewportWidth >= 768){
+        $('#signInModal').addClass('pater')
+        $('.nav-content').css('margin-left','0');
+        $('#signInModal').children().prevObject['0'].childNodes['7'].textContent = ''
+        $('#signInModal').children().prevObject['0'].childNodes['8'].textContent = ''
+    }
+});
--- menu/menu.php
+++ menu/menu.php
@@ -2,31 +2,40 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="../css/pater.css">
 	<title>OpenSpace</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
-<header id='hed'>
-	<div class="header_content content content--fixed">
-		<nav>
-			<ul class="snip1135">
-				<li><img src="../image/logo.png" class='logo' alt=""></li>
-				<li id="index"><a href="../main/index.php" data-hover="Главная">Главная</a></li>
-				<li id="poster"><a href="../poster/poster.php" data-hover="Афиша">Афиша</a></li>
-				<li id="cinemaPage"><a href="../cinemaPage/cinemaPage.php" data-hover="Кинотеатры">Кинотеатры</a></li>
-				<li id="contactPage"><a href="../contactPage/contactPage.php" data-hover="Контакты">Контакты</a></li>
-			</ul>
-		</nav>
-			<?php
-					echo "<a class='pater' id='signInModal' href='../register/reg.php'>";
-				echo "<div class='pater_text'>Войти</div>";
-			echo "<svg class='pater__deco' width='300' height='240' viewBox='0 0 1000 800'>
-				<path fill='#764098' d='M27.4,171.8C73,42.9,128.6,1,128.6,1s0,0,0,0c58.5,0,368.3,0.3,873.2,0.8c38.5,211,42.1,373.5,38.9,476.7c-2.5,80.3-10.6,174.9-76.7,247.8c-15.1,16.6-37.4,41.2-72.8,53.9c-92.4,33.1-173-50.8-283.9-99.4c-224.3-98.4-334.9,51.4-472.2-45.6C-6.3,535.2-14.5,290.6,27.4,171.8z'/>
-			</svg>";
-					echo "<p class='pater__desc'>";
-			echo 'Войти';
-			echo "<br><br>Войдите в свой аккаунт, чтобы заказать билет";
-			echo "</p>";
-				echo "</a>";
-				
-			?>
-
-	</div>
-</header>
+<header>
+        <nav>
+            <div class="nav-logo">
+                <img src="../image/logo.png" class='logo' alt="">
+            </div>
+            <div class="nav-content">
+                <div class="nav-list">
+                    <ul>
+                        <li><a href="#" data-hover="Главная">Главная</a></li>
+                        <li><a href="#" data-hover="Афиша">Афиша</a></li>
+                        <li><a href="#" data-hover="Акции">Акции</a></li>
+                        <li><a href="#" data-hover="Еда и напитки">Еда и напитки</a></li>
+                        <li><a href="#" data-hover="Контакты">Контакты</a></li>
+                    </ul>
+                </div>
+                <div class="nav-signIn">
+                    <a class='pater' id='signInModal' href='../register/reg.php' data-hover="ВОЙТИ" >
+                        <div class='pater_text'>ВОЙТИ</div>
+                        <svg class='pater__deco' width='300' height='240' viewBox='0 0 1000 800'>
+                            <path fill='#764098' d='M27.4,171.8C73,42.9,128.6,1,128.6,1s0,0,0,0c58.5,0,368.3,0.3,873.2,0.8c38.5,211,42.1,373.5,38.9,476.7c-2.5,80.3-10.6,174.9-76.7,247.8c-15.1,16.6-37.4,41.2-72.8,53.9c-92.4,33.1-173-50.8-283.9-99.4c-224.3-98.4-334.9,51.4-472.2-45.6C-6.3,535.2-14.5,290.6,27.4,171.8z'/>
+                        </svg>
+                        <p class='pater__desc'>
+                            ВОЙТИ
+                            <br><br>Войдите в свой аккаунт, чтобы заказать билет
+                        </p>
+                    </a>
+                </div>
+            </div>
+        </nav>
+        <div class="hamburger" id="hamburger-1">
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+        </div>
+    </header>
